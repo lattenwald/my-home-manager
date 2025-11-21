ecc65b9d7cb70d13a576d0cd4a7bf37db1ed89f2
Alexander Q <qalexx@gmail.com>
2025-11-21 15:15:31 +0400

    chore: switch to nixos-unstable and home-manager master

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "My Home Manage configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
